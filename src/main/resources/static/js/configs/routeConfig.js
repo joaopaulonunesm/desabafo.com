4d47e26db234a69979f9693d8962fe72e281b438
Joao Marques <joaopaulonunesm@hotmail.com>
2017-08-15 19:14:52 -0300

    Telas de TopDesabafos e TopAssuntos

--- src/main/resources/static/js/configs/routeConfig.js
+++ src/main/resources/static/js/configs/routeConfig.js
@@ -4,6 +4,18 @@ angular.module("desabafoApp").config(function ($routeProvider) {
 		templateUrl: "views/timeline.html",
 	});
 	
+	$routeProvider.when("/ultimos/assuntos", {
+		templateUrl: "views/ultimosAssuntos.html",
+	});
+	
+	$routeProvider.when("/top/assuntos", {
+		templateUrl: "views/topAssuntos.html",
+	});
+	
+	$routeProvider.when("/top/desabafos", {
+		templateUrl: "views/topDesabafos.html",
+	});
+	
 	$routeProvider.when("/assunto/:assunto", {
 		templateUrl: "views/assunto.html",
 		controller: "umAssuntoCtrl"
@@ -13,6 +25,7 @@ angular.module("desabafoApp").config(function ($routeProvider) {
 		templateUrl: "views/publicacao.html",
 		controller: "umaPublicacaoCtrl"
 	});
+	
 	
 	$routeProvider.otherwise({redirectTo: "/"});
 	
