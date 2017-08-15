a961b3e12472b22b8c356f783b790a4377355987
Joao Marques <joaopaulonunesm@hotmail.com>
2017-08-15 03:31:13 -0300

    Alteração de ambiente para Heroku

--- src/main/resources/static/js/configs/urlConfig.js
+++ src/main/resources/static/js/configs/urlConfig.js
@@ -1,4 +1,4 @@
 angular.module("desabafoApp").value("config", {
-	baseUrl: "http://localhost:8080",
-	//baseUrl : "https://appdesabafo.herokuapp.com",
+	//baseUrl: "http://localhost:8080",
+	baseUrl : "https://appdesabafo.herokuapp.com",
 });
